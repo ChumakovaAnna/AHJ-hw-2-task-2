b3baab72de38aa09ea88571d3e578e7603abe014
Anna Chumakova <chumakova3008@gmail.com>
2020-07-27 00:50:56 +0500

    Создан класс для фильтров таблицы (class Filters)

---
+++ src/js/Filters.js
@@ -0,0 +1,40 @@
+/**
+ * Класс для видов фильтрации Json
+ */
+export default class Filters {
+  constructor(json) {
+    this.json = json;
+  }
+
+  /**
+   * Фильтр по Id
+   */
+  sortById() {
+    const jsonId = this.json.slice().sort((a, b) => a.id - b.id);
+    return jsonId;
+  }
+
+  /**
+   * Фильтр по Title
+   */
+  sortByTitle() {
+    const jsonTitle = this.json.slice().sort((a, b) => a.title - b.title);
+    return jsonTitle;
+  }
+  /**
+   * Фильтр по Year
+   */
+
+  sortByYear() {
+    const jsonYear = this.json.slice().sort((a, b) => a.year - b.year);
+    return jsonYear;
+  }
+
+  /**
+   * Фильтр по Imdb
+   */
+  sortByImdb() {
+    const jsonImdb = this.json.slice().sort((a, b) => a.imdb - b.imdb);
+    return jsonImdb;
+  }
+}

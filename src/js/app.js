60bba8f19f5a88e33d8f41cdf21ef37597f5a4af
Anna Chumakova <chumakova3008@gmail.com>
2020-07-30 23:57:36 +0500

    Создан файл для запуска проекта

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
+import TableController from "./TableController";
+import getJson from "./getJson";
+import Filters from "./Filters";
+import ChangeTable from "./ChangeTable";
+
 console.log("it works!");
+
+const json = getJson();
+const table = new TableController();
+table.bindToDOM(document.querySelector(".container"));
+
+const filters = new Filters(json);
+const changeTable = new ChangeTable(table, filters);
+changeTable.changingTable();

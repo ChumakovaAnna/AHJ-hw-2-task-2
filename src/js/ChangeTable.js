a1d097fce900d6dd2d79ce5b6a7d36a8710f0045
Anna Chumakova <chumakova3008@gmail.com>
2020-07-30 23:41:31 +0500

    Добавлен класс для изменения таблицы по интервалу

---
+++ src/js/ChangeTable.js
@@ -0,0 +1,46 @@
+/**
+ * Класс для изменения таблицы по интервалу
+ */
+export default class ChangeTable {
+  /**
+   * @param  {} TableController - класс для прорисовки игрового поля
+   * @param  {} Filters - класс для видов фильтрации Json
+   */
+  constructor(TableController, Filters) {
+    this.table = TableController;
+    this.filters = Filters;
+    this.json = null;
+  }
+
+  /**
+   * Метод для изменения порядка в  таблицы в каждом столбце по интервалу
+   */
+  changingTable() {
+    console.log("Сортировка по id");
+    this.json = this.filters.sortById();
+    this.table.getData(this.json);
+    this.table.drawArrow("id");
+    this.table.drawUi();
+
+    setTimeout(() => {
+      console.log("Сортировка по title");
+      this.json = this.filters.sortByTitle();
+      this.table.getData(this.json);
+      this.table.drawUi();
+    }, 5000);
+
+    setTimeout(() => {
+      console.log("Сортировка по year");
+      this.json = this.filters.sortByYear();
+      this.table.getData(this.json);
+      this.table.drawUi();
+    }, 10000);
+
+    setTimeout(() => {
+      console.log("Сортировка по imdb");
+      this.json = this.filters.sortByImdb();
+      this.table.getData(this.json);
+      this.table.drawUi();
+    }, 15000);
+  }
+}
